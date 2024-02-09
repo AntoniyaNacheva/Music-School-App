2ed4fe2d6ae11f6f1fc4ccef459108e1253bf851
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-02-09 15:20:32 +0200

    Footer

--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -1,6 +1,14 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
-	return <div>Footer</div>;
-  };
-  
-  export default Footer;
-  
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        All rights reserved © 2024 Music Center Blagoevgrad, Bulgaria
+      </div>
+      <div>Project of Antoniya Nacheva</div>
+    </div>
+  );
+};
+
+export default Footer;
