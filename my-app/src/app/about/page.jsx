21b15968877f506592df17f0b32825a026727b74
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-02-09 10:44:54 +0200

    Next.js <Image />

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-  return <div>За Нас</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src="/kid.png" alt="" fill/>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
